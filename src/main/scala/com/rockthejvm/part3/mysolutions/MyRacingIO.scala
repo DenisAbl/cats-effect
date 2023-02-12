ea29d69e372836dcf98e57a65e0dc666c1829124
Denis Ablamskiy <ablamskiy.d@gmail.com>
2023-02-12 22:47:17 +0100

    Updates

--- src/main/scala/com/rockthejvm/part3/mysolutions/MyRacingIO.scala
+++ src/main/scala/com/rockthejvm/part3/mysolutions/MyRacingIO.scala
@@ -1,5 +1,6 @@
 package com.rockthejvm.part3.mysolutions
 
+import cats.Traverse
 import cats.effect.IO.{IOCont, Uncancelable}
 import cats.effect.{Fiber, IO, IOApp, Outcome}
 
@@ -66,11 +67,45 @@ object MyRacingIO extends IOApp.Simple {
   // 2
   def unrace[A, B](ioa: IO[A], iob: IO[B]): IO[Either[A, B]] =
     IO.racePair(ioa, iob).flatMap {
-
+      case Left((aRes, bFib)) => bFib.join.flatMap {
+        case Outcome.Succeeded(fb) => fb.map(b => Right(b))
+        case Outcome.Errored(e) => IO.raiseError(e)
+        case Outcome.Canceled() => IO.raiseError(new Exception ("Last comp was canceled"))
+      }
+      case Right((aFib, bRes)) => aFib.join.flatMap {
+        case Outcome.Succeeded(fa) => fa.map(a => Left(a))
+        case Outcome.Errored(e) => IO.raiseError(e)
+        case Outcome.Canceled() => IO.raiseError(new Exception ("Last comp was canceled"))
+      }
     }
 
 
   // 3
-  def simpleRace[A, B](ioa: IO[A], iob: IO[B]): IO[Either[A, B]] = ???
-  override def run: IO[Unit] = ???
+  def simpleRace[A, B](ioa: IO[A], iob: IO[B]): IO[Either[A, B]] = {
+    IO.racePair(ioa, iob).flatMap {
+      case Left((aRes, fiberB)) => aRes match {
+        case Outcome.Succeeded(fa) => fa.map(a => Left(a))
+        case Outcome.Errored(e) => IO.raiseError(e)
+        case Outcome.Canceled() => fiberB.join.flatMap{
+          case Outcome.Succeeded(fb) => fb.map(b => Right(b))
+          case Outcome.Errored(e) => IO.raiseError(e)
+          case Outcome.Canceled() => IO.raiseError(new Exception ("ioA and ioB was canceled"))
+        }
+      }
+      case Right((fibA, bRes)) => bRes match {
+        case Outcome.Succeeded(fb) => fb.map(b => Right(b))
+        case Outcome.Errored(e) => IO.raiseError(e)
+        case Outcome.Canceled() => fibA.join.flatMap {
+          case Outcome.Succeeded(fa) => fa.map(a => Left(a))
+          case Outcome.Errored(e) => IO.raiseError(e)
+          case Outcome.Canceled() => IO.raiseError(new Exception("ioA and ioB was canceled"))
+        }
+      }
+    }
+  }
+  override def run: IO[Unit] = {
+    val ioa = IO("AAAAAAAAAAA").debug >> IO.sleep(5.seconds) >> IO("Finished AAA").debug
+    val iob = IO("BBBBBBBBBBB").debug >> IO.sleep(7.seconds) >> IO.canceled
+    IO.race(ioa, iob) >> IO.unit
+  }
 }
